db03f4e07ab843e2fbe9114320bff41eebe054ac
WeizhenWang-1210 <wwz011210@g.ucla.edu>
2023-10-10 22:30:13 -0700

    Working on English generation

--- question_generation.py
+++ question_generation.py
@@ -6,8 +6,9 @@ import glob
 from typing import Callable, Any, Iterable
 from collections import defaultdict
 OPENAI_KEY = "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"
-from question_generator import Query,SubQuery,count
+from question_generator import Query,SubQuery,count, QueryAnswerer,CountGreater, CountEqual, CountLess
 from scene_graph import SceneGraph
+from agent_node import nodify
 #don't work when the scene is crowded.
 
 """
@@ -41,8 +42,12 @@ class QuestionSpecifier:
 
 
     def translate_to_En(self):
-        if self.type["format"] == "Counting":
-            return counting_translator()("white cars")
+        format = self.type["format"]
+        if format == "count":
+            return counting_translator()(QtoEn(self.type["paths"]))
+        elif format == "logical":
+            translator = decode_predicate(self.type["end"])
+            return translator(QtoEn(self.type["paths"]))
         return ""
         
 
@@ -58,7 +63,14 @@ class QuestionSpecifier:
         def select_end_filter(type):
             if type == "count":
                 return count
-            return lambda x: x
+            elif type == "Count Greater":
+                return CountGreater
+            elif type == "Count Equal":
+                return CountEqual
+            elif type == "Count Less":
+                return CountLess
+            else:
+                return lambda x: x
         
         paths = self.type["paths"]
         subqueries = []
@@ -68,45 +80,108 @@ class QuestionSpecifier:
                 partial.append(SubQuery(sub["color"], sub["type"], sub["pos"]))
             hookup(partial)
             subqueries.append(partial)
-        query = Query([sub[0] for sub in subqueries],self.type["format"],select_end_filter(self.type["format"]),
+        query = Query([sub[0] for sub in subqueries],self.type["format"],select_end_filter(self.type["end"]),
                       self.graph.get_ego_node().heading, 
                       self.graph.get_nodes())
         return query
-            
-            
+                
+
+def QtoEn(paths):
+    result = []
+    for path in paths[::-1]:
+        string = ""
+        for q_spec in path[::-1]:
+            string += subQtoEn(q_spec)
+        result.append(string)
+    if len(result) == 1:
+        result = result[0]
+    return result
+
+def subQtoEn(spec):
+    #print(spec)
+    pos_string = decode_pos(spec["pos"])
+    color_string = decode_color(spec["color"])
+    type_string = decode_type(spec["type"])
+    return "{}{}{}".format(color_string, type_string, pos_string)
+
+def decode_pos(pos_strings):
+    if not pos_strings:
+        return ""
+    direction_string = pos_strings[0]
+    prefix = "that is "
+    modifier = ""
+    if direction_string == 'l':
+        modifier = 'directly to the left of '
+    elif direction_string == 'lf':
+        modifier = 'to the left and in front of '
+    elif  direction_string == 'lb':
+        modifier = 'to the left and behind '
+    elif  direction_string == 'r':
+        modifier = 'directly to the right of '
+    elif  direction_string == 'rf':
+        modifier = 'to the right and in front of '
+    elif  direction_string == 'rb':
+        modifier = 'to the right and behind '
+    elif  direction_string == 'b':
+        modifier = 'directly behind '
+    else:
+        modifier = 'directly in front of '
+    return prefix + modifier
+
+def decode_color(colors):
+    if not colors:
+        return ""
+    return colors[0].lower() + " "
+
+def decode_type(types):
+    if not types:
+        return ""
+    return types[0].lower() + " "
+
+def decode_predicate(type):
+    if type == "Count Larger":
+        return lambda x : "Are there more {}than {}?".format(x[0],x[1])
+    elif type == "Count Equal":
+        return lambda x : "Are there equal number of {} and {}?".format(x[0],x[1])
+    else:
+        return lambda x : "Are there less {}than {}?".format(x[0],x[1])
+
+
+
+
 
 
 def counting_translator():
     def func(query_string):
-        result = "How many {} are visible and lidar detectable?".format(query_string)
+        result = "How many {}are visible and lidar detectable?".format(query_string)
         return result
     return func
 
 
 
 
-dict(
-    format = "Logical",
+logic_example = dict(
+    format = "logical",
     paths = [
         [
             dict(
-                type = ["vehicle"],
+                type = ["Vehicle"],
                 color = ["White"],
-                position = None),
+                pos = None),
             dict(
-                type = ["sportscar"],
+                type = ["Vehicle"],
                 color = ["Grey"],
-                position = ["f"]
-            )
+                pos = ["l"]),
+
         ],
         [
             dict(
-            type = ["vehicle"],
+            type = ["Vehicle"],
             color = ["Red"],
-            position = None)
+            pos = None)
         ]
     ],
-    end = "Count Larger"
+    end = "Count Less"
 )
 # Is there more grey sportscar that is in front of white vehicles than red vehicles
 # <end>  <subq2,q1>                           <subq1,q1>           <end> <q2>
@@ -114,29 +189,43 @@ dict(
 
 
 counting_example = dict(
-    format = "Counting",
+    format = "count",
     paths =  [
         [
             dict(
-                type = ["vehicle"],
+                type = ["Vehicle"],
                 color = ["White"],
-                position = None),
+                pos = ["lf"]),
             dict(
-                type = ["sportscar"],
+                type = ["Sportscar"],
                 color = ["Grey"],
-                position = ["f"]
+                pos = ["l"]
             )
         ]
     ],
-    end = "counting"
+    end = "count"
 )
 
 #How many grey sportscar that is in front of white vehicles are visible and lidar detectable?
 #   <end>                     <q1>                                         <end>
 
 if __name__ == "__main__":
-    test = QuestionSpecifier(counting_example)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--step", type=str, default = "520/520_60/world_520_60")
+    args = parser.parse_args()
+    try:
+        print(args.step)
+        with open('{}.json'.format(args.step),'r') as scene_file:
+                scene_dict = json.load(scene_file)
+    except:
+        print("Wrong")
+    agent_id,nodes = nodify(scene_dict)
+    graph = SceneGraph(agent_id,nodes)  
+    test = QuestionSpecifier(counting_example, graph)
+    prophet = QueryAnswerer(graph, [test.translate_to_Q()])
     print(test.translate_to_En())
+    print(prophet.ans())
+    #print(test.translate_to_Q())
 
 
 
