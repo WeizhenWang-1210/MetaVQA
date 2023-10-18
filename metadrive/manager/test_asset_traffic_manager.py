6f97bf7f78777fbfb481b5baee08f1815e9783a5
Chenda Duan <chenda@ucla.edu>
2023-10-17 23:25:04 -0700

    Modify traffic manager

--- metadrive/manager/test_asset_traffic_manager.py
+++ metadrive/manager/test_asset_traffic_manager.py
@@ -6,6 +6,7 @@ from typing import Dict
 import math
 import os
 import json
+import random
 import numpy as np
 from asset.read_config import configReader
 from metadrive.component.lane.abs_lane import AbstractLane
@@ -13,6 +14,7 @@ from metadrive.component.map.base_map import BaseMap
 from metadrive.component.pgblock.first_block import FirstPGBlock
 from metadrive.component.road_network import Road
 from metadrive.component.vehicle.base_vehicle import BaseVehicle
+from metadrive.component.vehicle.vehicle_type import CustomizedCar
 from metadrive.constants import TARGET_VEHICLES, TRAFFIC_VEHICLES, OBJECT_TO_AGENT, AGENT_TO_OBJECT
 from metadrive.manager.base_manager import BaseManager
 from metadrive.manager.traffic_manager import PGTrafficManager
@@ -36,32 +38,23 @@ class TestAssetTrafficManager(BaseManager):
     VEHICLE_GAP = 10  # m
 
     SPAW_LANE_INDEX = (FirstPGBlock.NODE_1, FirstPGBlock.NODE_2, 0)
-    def _propose_vehicle_configs(self, lane: AbstractLane):
-        potential_vehicle_configs = []
-        total_num = int(lane.length / self.VEHICLE_GAP)
-        vehicle_longs = [i * self.VEHICLE_GAP for i in range(total_num)]
-        # Only choose given number of vehicles
-        for long in vehicle_longs:
-            random_vehicle_config = {"spawn_lane_index": lane.index, "spawn_longitude": long, "enable_reverse": False}
-            potential_vehicle_configs.append(random_vehicle_config)
-        return potential_vehicle_configs
-    def _init_additional_model(self):
+    def __init__(self):
+        super().__init__()
         self.config = configReader()
         self.path_config = self.config.loadPath()
-        self.adj_params_folder = self.path_config["adj_parameter_folder"]
-        self.car_configs = []  # List to store the file paths
-        for root, dirs, files in os.walk(self.adj_params_folder ):
+
+    def init_car_adj_list(self):
+        self.car_asset_metainfos = []  # List to store the file paths
+        for root, dirs, files in os.walk(self.adj_folder):
             for file in files:
                 if file.lower().startswith("car"):
-                    os.path.join(root, file)
                     with open(os.path.join(root, file), 'r') as file:
                         loaded_metainfo = json.load(file)
-                        loaded_metainfo["MODEL_SCALE"] = tuple(loaded_metainfo["MODEL_SCALE"])
-                        loaded_metainfo["MODEL_OFFSET"] = tuple(loaded_metainfo["MODEL_OFFSET"])
-                        loaded_metainfo["MODEL_SCALE"] = tuple(loaded_metainfo["MODEL_SCALE"])
-                    self.car_configs.append(loaded_metainfo)
-    def randomCustomizedCar(self):
+                        print(loaded_metainfo)
+                        self.car_asset_metainfos.append(loaded_metainfo)
 
+    def randomCustomizedCar(self):
+         return CustomizedCar, random.choice(self.car_asset_metainfos)
     def _create_vehicles_once(self, map: BaseMap, traffic_density: float) -> None:
         """
         Trigger mode, vehicles will be triggered only once, and disappear when arriving destination
@@ -76,11 +69,17 @@ class TestAssetTrafficManager(BaseManager):
         from metadrive.policy.idm_policy import IDMPolicy
         vehicles_on_block = []
         for v_config in selected:
-            vehicle_type = self.random_vehicle_type()
+            vehicle_type, asset_info = self.randomCustomizedCar()
             v_config.update(self.engine.global_config["traffic_vehicle_config"])
-            random_v = self.spawn_object(vehicle_type, vehicle_config=v_config)
+            random_v = self.spawn_object(vehicle_type, vehicle_config=v_config, test_asset_meta_info=asset_info)
             self.add_policy(random_v.id, IDMPolicy, random_v, self.generate_seed())
             vehicles_on_block.append(random_v.name)
+
+            # vehicle_type = self.random_vehicle_type()
+            # v_config.update(self.engine.global_config["traffic_vehicle_config"])
+            # random_v = self.spawn_object(vehicle_type, vehicle_config=v_config)
+            # self.add_policy(random_v.id, IDMPolicy, random_v, self.generate_seed())
+            # vehicles_on_block.append(random_v.name)
 
         trigger_road = FirstPGBlock.pre_block_socket.positive_road
         block_vehicles = BlockVehicles(trigger_road=trigger_road, vehicles=vehicles_on_block)
