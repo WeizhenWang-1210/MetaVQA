0b796fd0f24787db5eb8337eeef5e7c9ec10c19e
WeizhenWang-1210 <wwz011210@g.ucla.edu>
2023-11-30 13:29:30 -0800

    refactoring + support for logical questions with more than two threads

--- vqa/question_generation.py
+++ vqa/question_generation.py
@@ -42,43 +42,113 @@ class QuestionSpecifier: # 1-to-1 corresponding relationship with a particular q
     
     """
     def __init__(self,
-                 specification,
+                 config,
                  graph: SceneGraph) -> None:
         
         """
         In the current English translation implementation, only can't generate fluent English like "Green Car or Red Car". In 
         addition, can only generate logical questions with two threads.
         """
-        self.type = specification
+        self.config= config
         self.graph = graph
 
     def translate_to_En(self):
         """
         Generate the corresponding English Question.
         """
-        format = self.type["format"]
+        #TODO Add more templates to the same question for diversity
+        #The translator is a kind of function that takes in an str and supply it to a predefined question template
+        format = self.config["format"]
         if format == "count":
             translator = counting_translator_generator()
         elif format == "logical":
-            translator = decode_predicate(self.type["end"])
+            def decode_predicate(type):
+                '''
+                Convert predicate functional programs to answers
+                '''
+                if type == "Count Greater":
+                    return lambda x : "Are there more {}than {}?".format(x[0],x[1])
+                elif type == "Count Equal":
+                    def func(x):
+                        if len(x) == 2:
+                            str = "and ".join(x)
+                        else:
+                            listing = ",".join(x)
+                            str = "the following:{}".format(listing)
+                        return "Are there equal number of {}?".format(str)
+                    return func
+                else:
+                    return lambda x : "Are there less {}than {}?".format(x[0],x[1])
+            translator = decode_predicate(self.config["end"])
         elif format == "localize":
             translator = localization_translator_generator()
-        return translator(self.QtoEn(self.type["paths"]))
+        return translator(self.QtoEn(self.config["paths"]))
     
     def QtoEn(self,paths):
         """
-        Convert List[List[dict]] into English. 
+        Convert List[List[dict]] into English description of the objects referred. 
         """
         result = []
         for path in paths:
             string = ""
             for q_spec in path[::-1]:
-                string += subQtoEn(q_spec)
+                string += self.subQtoEn(q_spec)
             string += "us " if path[0]["pos"] else ""
             result.append(string)
         if len(result) == 1:
             result = result[0]
         return result
+    
+    def subQtoEn(self,spec):
+        """
+        Takes dict(type = , color = , pos = ) and convert this into English
+        """
+        #TODO create a dictionary that the function can consult
+        def decode_pos(pos_strings):
+            """
+            Convert str-encoded pos_strings into English
+            """
+            if not pos_strings:
+                return ""
+            direction_string = pos_strings[0]
+            prefix = "that is "
+            modifier = ""
+            if direction_string == 'l':
+                modifier = 'directly to the left of '
+            elif direction_string == 'lf':
+                modifier = 'to the left and in front of '
+            elif  direction_string == 'lb':
+                modifier = 'to the left and behind '
+            elif  direction_string == 'r':
+                modifier = 'directly to the right of '
+            elif  direction_string == 'rf':
+                modifier = 'to the right and in front of '
+            elif  direction_string == 'rb':
+                modifier = 'to the right and behind '
+            elif  direction_string == 'b':
+                modifier = 'directly behind '
+            else:
+                modifier = 'directly in front of '
+            return prefix + modifier
+
+        def decode_color(colors):
+            """Convert string-encoded color into English"""
+            if not colors:
+                return ""
+            return colors[0].lower() + " "
+
+        def decode_type(types):
+            """
+            Convert string-encoded type into English
+            """
+            if not types:
+                return "thing" + " "
+            return types[0].lower() + " "
+
+        pos_string = decode_pos(spec["pos"])
+        color_string = decode_color(spec["color"])
+        type_string = decode_type(spec["type"])
+        return "{}{}{}".format(color_string, type_string, pos_string)
     
     def translate_to_Q(self):
         '''
@@ -106,7 +176,7 @@ class QuestionSpecifier: # 1-to-1 corresponding relationship with a particular q
                 func = locate_wrapper(self.graph.get_ego_node())
                 return func
         
-        paths = self.type["paths"]
+        paths = self.config["paths"]
         subqueries = []
         for path in paths:
             partial = []
@@ -114,14 +184,24 @@ class QuestionSpecifier: # 1-to-1 corresponding relationship with a particular q
                 partial.append(SubQuery(sub["color"], sub["type"], sub["pos"]))
             hookup(partial)
             subqueries.append(partial)
-        query = Query([sub[0] for sub in subqueries],self.type["format"],select_end_filter(self.type["end"]),
+        query = Query([sub[0] for sub in subqueries],self.config["format"],select_end_filter(self.config["end"]),
                       self.graph.get_ego_node().heading, 
                       self.graph.get_nodes())
         return query
         
 
-    
-                
+"""
+config = dict(
+    format = "", #str
+    paths = [   #Iterable[Iterable]
+        [],
+        [],
+        []
+    ],
+    end = "" #str
+)
+
+"""           
 
 
 def localization_translator_generator():
@@ -132,78 +212,6 @@ def localization_translator_generator():
         result = "Locate the {}".format(query_string)
         return result
     return func
-
-
-
-
-
-
-def subQtoEn(spec):
-    """
-    Takes dict(type = , color = , pos = ) and convert this into English
-    """
-    #print(spec)
-    pos_string = decode_pos(spec["pos"])
-    color_string = decode_color(spec["color"])
-    type_string = decode_type(spec["type"])
-    return "{}{}{}".format(color_string, type_string, pos_string)
-
-def decode_pos(pos_strings):
-    """
-    Convert str-encoded pos_strings into English
-    """
-    if not pos_strings:
-        return ""
-    direction_string = pos_strings[0]
-    prefix = "that is "
-    modifier = ""
-    if direction_string == 'l':
-        modifier = 'directly to the left of '
-    elif direction_string == 'lf':
-        modifier = 'to the left and in front of '
-    elif  direction_string == 'lb':
-        modifier = 'to the left and behind '
-    elif  direction_string == 'r':
-        modifier = 'directly to the right of '
-    elif  direction_string == 'rf':
-        modifier = 'to the right and in front of '
-    elif  direction_string == 'rb':
-        modifier = 'to the right and behind '
-    elif  direction_string == 'b':
-        modifier = 'directly behind '
-    else:
-        modifier = 'directly in front of '
-    return prefix + modifier
-
-def decode_color(colors):
-    """Convert string-encoded color into English"""
-    if not colors:
-        return ""
-    return colors[0].lower() + " "
-
-def decode_type(types):
-    """
-    Convert string-encoded type into English
-    """
-    if not types:
-        return ""
-    return types[0].lower() + " "
-
-def decode_predicate(type):
-    '''
-    Convert predicate functional programs to answers
-    '''
-    if type == "Count Greater":
-        return lambda x : "Are there more {}than {}?".format(x[0],x[1])
-    elif type == "Count Equal":
-        return lambda x : "Are there equal number of {} and {}?".format(x[0],x[1])
-    else:
-        return lambda x : "Are there less {}than {}?".format(x[0],x[1])
-
-
-
-
-
 
 def counting_translator_generator():
     '''
@@ -229,6 +237,12 @@ logic_example = dict(
         [
             dict(
             type = ["Policecar"],
+            color = ["Black"],
+            pos = None)
+        ],
+        [
+            dict(
+            type = ["Vehicle"],
             color = ["Black"],
             pos = None)
         ]
@@ -298,12 +312,13 @@ localization_example = dict(
 )
 
 
+
 #How many grey sportscar that is in front of white vehicles are visible and lidar detectable?
 #   <end>                     <q1>                                         <end>
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--step", type=str, default = "some_folder/10_40/world_10_40")
+    parser.add_argument("--step", type=str, default = "verification/10_40/world_10_40")
     args = parser.parse_args()
     try:
         print(args.step)
