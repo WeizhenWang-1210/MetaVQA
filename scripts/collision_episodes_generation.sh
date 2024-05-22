13a96320df7f75eaf451763917a1f6a497bb44d9
Weizhen <wwz011210@g.ucla.edu>
2024-05-21 23:38:01 -0700

    script for generating collision episodes scenarios.

--- scripts/collision_episodes_generation.sh
+++ scripts/collision_episodes_generation.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 paths=(
-    "/bigdata/yuxin/cat_reconstructed/train/subdir_21"
-    "/bigdata/yuxin/cat_reconstructed/train/subdir_22/"
-    "/bigdata/yuxin/cat_reconstructed/train/subdir_23/"
-    #"/bigdata/yuxin/cat_reconstructed/train/subdir_24/",
-    #"/bigdata/yuxin/cat_reconstructed/train/subdir_25/",
-    #"/bigdata/yuxin/cat_reconstructed/train/subdir_26/",
-    #"/bigdata/yuxin/cat_reconstructed/train/subdir_27/",
+    #"/bigdata/yuxin/cat_reconstructed/train/subdir_21"
+    #"/bigdata/yuxin/cat_reconstructed/train/subdir_22"
+    #"/bigdata/yuxin/cat_reconstructed/train/subdir_23"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_24"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_25"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_26"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_27"
 )
+
 
 
 for path in "${paths[@]}"; do
     if [ -d "$path" ]; then
-        echo "Working on $path$."
+        echo "Working on $path."
         python -m vqa.multiprocess_episodes_generation --headless --num_proc 32 \
         --scenarios --data_directory "$path" --source "Waymo_CAT" --split "train"
     else
