6f521ed8e6ad66ea57dc654896bbcab0c53e9d11
Weizhen <weizhen@bolei-gpu06.cs.ucla.edu>
2024-05-25 15:34:22 -0700

    New script for generating nuscenes scenarios.

--- scripts/collision_safety_question_generation.sh
+++ scripts/collision_safety_question_generation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 python -m vqa.multiprocess_question_generation --num_proc 32 --src "Waymo_CAT" --root_directory \
-"/bigdata/weizhen/metavqa_final/scenarios/training/safety_critical" --output_base \
-"/bigdata/weizhen/metavqa_final/vqa/training/safety_critical/collision"
+"/bigdata/weizhen/metavqa_final/scenarios/testing/safety_critical" --output_base \
+"/bigdata/weizhen/metavqa_final/vqa/testing/safety_critical/collision"
