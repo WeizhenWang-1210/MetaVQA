5b085f884b675f54fcb866a0d3ae3b883f3064d0
Weizhen <wwz011210@g.ucla.edu>
2024-06-26 10:49:37 -0700

    Local changes.

--- scripts/question_generation.sh
+++ scripts/question_generation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-python -m vqa.multiprocess_question_generation --num_proc 32 --src "NuScenes" --root_directory \
+python -m vqa.multiprocess_question_generation --num_proc 8 --src "NuScenes" --root_directory \
 "/bigdata/weizhen/metavqa_final/scenarios/testing/normal/sc_nusc_trainval_6" --output_base \
-"/bigdata/weizhen/metavqa_final/vqa/testing/single_frame/NuScenes/"
+"/bigdata/weizhen/metavqa_final/vqa/testing/multi_frame/NuScenes" ;
+python -m
