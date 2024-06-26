a70f8addd7295ed7bee7e0c7f5eb9b1cd787737d
weizhen <wwz011210@g.ucla.edu>
2024-06-26 13:29:38 -0700

    fix typo.

--- scripts/question_generation.sh
+++ scripts/question_generation.sh
@@ -3,4 +3,3 @@
 python -m vqa.multiprocess_question_generation --num_proc 8 --src "NuScenes" --root_directory \
 "/bigdata/weizhen/metavqa_final/scenarios/testing/normal/sc_nusc_trainval_6" --output_base \
 "/bigdata/weizhen/metavqa_final/vqa/testing/multi_frame/NuScenes" ;
-python -m
