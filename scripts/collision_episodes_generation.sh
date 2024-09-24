d1c45a75382761f41eea2dc02099a14bf5ae1f35
Weizhen <weizhen@bolei-gpu06.cs.ucla.edu>
2024-09-24 16:48:42 -0700

    scripts for generating cat episodes.

--- scripts/collision_episodes_generation.sh
+++ scripts/collision_episodes_generation.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 paths=(
-    #"/bigdata/yuxin/cat_reconstructed/train/subdir_21"
-    #"/bigdata/yuxin/cat_reconstructed/train/subdir_22"
-    #"/bigdata/yuxin/cat_reconstructed/train/subdir_23"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_4"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_7"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_11"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_12"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_13"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_14"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_15"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_17"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_18"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_19"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_20"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_21"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_22"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_23"
     "/bigdata/yuxin/cat_reconstructed/train/subdir_24"
     "/bigdata/yuxin/cat_reconstructed/train/subdir_25"
     "/bigdata/yuxin/cat_reconstructed/train/subdir_26"
     "/bigdata/yuxin/cat_reconstructed/train/subdir_27"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_28"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_29"
+    "/bigdata/yuxin/cat_reconstructed/train/subdir_30"
 )
 
 
@@ -14,8 +28,9 @@ paths=(
 for path in "${paths[@]}"; do
     if [ -d "$path" ]; then
         echo "Working on $path."
-        python -m vqa.multiprocess_episodes_generation --headless --num_proc 32 \
-        --scenarios --data_directory "$path" --source "Waymo_CAT" --split "train"
+        python -m vqa.multiprocess_episodes_generation --headless --num_proc 1 \
+        --scenarios --data_directory "$path" --source "Waymo_CAT" --split "train" --config "./vqa/configs/cat_scene.yaml"
+
     else
         echo "File $path does not exist."
     fi
