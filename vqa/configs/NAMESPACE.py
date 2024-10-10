41f39a2f2a128716e5728c079672b72471054932
weizhen <wwz011210@.ucla.edu>
2024-10-09 17:52:02 -0700

    namespace for options generation

--- vqa/configs/NAMESPACE.py
+++ vqa/configs/NAMESPACE.py
@@ -1,14 +1,18 @@
-namespace = dict(
+NAMESPACE = dict(
     type = [
-        "SUV", "Sedan", "Truck", "Sportscar","Jeep","Pickup","Compact Sedan",
-        "Traffic Cone", "Warning sign", "Planar Barrier", "Vehicle", "Pedestrian",
-        "Traffic Participant","Traffic Obstacle",None
+        "SUV", "sedan", "truck", "sportscar","jeep","pickup","Compact Sedan",
+        "Traffic Cone", "Warning Sign", "Planar Barrier", "Vehicle", "Pedestrian",
+        "Traffic Participant","Traffic Obstacle"
     ],
     color = [
-        "White","Black","Grey","Red","Blue","Orange","Yellow","Green",None
+        "White","Black","Grey","Red","Blue","Orange","Yellow","Green"
     ],
     pos = [
-        "l","f","r","b","lf","rf","lb","rb",None
+        "l","f","r","b","lf","rf","lb","rb"
     ],
 
+)
+
+POSITION2CHOICE = dict(
+    lf="left-front", rf="right-front", lb="left-back", rb="right-back", f="front", b="back", l="left", r="right"
 )
