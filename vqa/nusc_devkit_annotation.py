56736f53c5166a2ab4c01ddcabd75527cba95594
weizhen <wwz011210@.ucla.edu>
2024-10-22 13:27:15 -0700

    nusc devkit basically completed.

--- vqa/nusc_devkit_annotation.py
+++ vqa/nusc_devkit_annotation.py
@@ -8,10 +8,14 @@ import copy
 from nuscenes.utils.data_classes import LidarPointCloud, RadarPointCloud, Box
 from nuscenes.utils.geometry_utils import view_points, box_in_image, BoxVisibility
 from pyquaternion import Quaternion
+from vqa.configs.NAMESPACE import MIN_OBSERVABLE_PIXEL
+from masking import find_areas
 import json
 import os
 from nuscenes.eval.common.utils import quaternion_yaw
 from PIL import Image
+
+#TODO  FURTHER FILTER BY OBSERVABLE PIXELS.
 
 ALL_TYPE = {
     "noise": 'noise',
@@ -48,9 +52,23 @@ ALL_TYPE = {
     "vehicle.ego": "ego"
 }
 
+METAVQA_TYPE={
 
 
+}
 
+
+def normalize_point(shape, point):
+    new_point = list(point)
+    if point[0] < 0:
+        new_point[0] = 0
+    elif point[0] > shape[0]:
+        new_point[0] = shape[0]
+    if point[1] < 0:
+        new_point[1] = 0
+    elif point[1] > shape[1]:
+        new_point[1] = shape[1]
+    return tuple(new_point)
 def find_extremeties(points):
     xmin = np.min(points[:, 0])
     xmax = np.max(points[:, 0])
@@ -58,20 +76,11 @@ def find_extremeties(points):
     ymax = np.max(points[:, 1])
     max_area = abs((ymin - ymax) * (xmin - xmax))
     return (xmin, ymin), (xmax, ymin), (xmax, ymax), (xmin, ymax), max_area
+
+
 def create_mask(shape, points):
-    def normalize_point(point):
-        new_point = list(point)
-        if point[0] < 0:
-            new_point[0] = 0
-        elif point[0] > shape[0]:
-            new_point[0] = shape[0]
-        if point[1] < 0:
-            new_point[1] = 0
-        elif point[1] > shape[1]:
-            new_point[1] = shape[1]
-        return tuple(new_point)
     # Create a path using the given points
-    points = [normalize_point(point) for point in points]
+    points = [normalize_point(shape, point) for point in points]
     result = find_extremeties(np.array(points))
     normalized_area = result[-1]
     path = Path(points)
@@ -81,6 +90,10 @@ def create_mask(shape, points):
     # Find points inside the path
     mask = path.contains_points(coords).reshape(shape)
     return mask, normalized_area
+
+
+
+
 def func(nusc, ego_pose_token, boxes, visibilities, camera_intrinsic):
     """
     boxes need to be in ego-sensor-centric view
@@ -96,6 +109,7 @@ def func(nusc, ego_pose_token, boxes, visibilities, camera_intrinsic):
     )
     individual_masks = []
     color_mapping = {}
+    corners_mapping = {}
     for idx, pair in enumerate(ordered_boxes):
         old_idx, box = pair
         if not visibilities[old_idx]:
@@ -121,10 +135,11 @@ def func(nusc, ego_pose_token, boxes, visibilities, camera_intrinsic):
         mask_shape = (900, 1600)  # Size of the mask
         mask, normalized_area = create_mask(mask_shape, flipped_points)
         # print(max_area, normalized_area, round(normalized_area/max_area,2))
-        if normalized_area / max_area < 0.5:
+        if normalized_area / max_area < 0.5 or normalized_area < MIN_OBSERVABLE_PIXEL:
             visibilities_copy[old_idx] = False
             continue
         color_mapping[old_idx] = color
+        corners_mapping[old_idx] = [normalize_point(mask_shape, point) for point in flipped_points]
         image = np.zeros((mask.shape[0], mask.shape[1], 3), dtype=np.uint8)
         image[mask] = color  # [:3]
         # image = image.reshape(height, width, 3)
@@ -136,21 +151,34 @@ def func(nusc, ego_pose_token, boxes, visibilities, camera_intrinsic):
         bmask_3d = np.stack([bmask] * 3, axis=-1)
         final_result = np.where(bmask_3d, individual_mask, final_result)
     combined_image_pil = Image.fromarray(final_result.astype('uint8'))
+
+    #further processing to account for observable pixels after occlusion.
+    areas, masks = find_areas(img=final_result.astype('uint8'), colors= [tuple(val) for val in color_mapping.values()])
+    new_color_mapping = {}
+    new_corners_mapping = {}
+    for idx, (key, value) in enumerate(color_mapping.items()):
+        if areas[idx] >= MIN_OBSERVABLE_PIXEL:
+            new_color_mapping[key] = value
+            new_corners_mapping[key] = corners_mapping[key]
+        else:
+            visibilities_copy[key] = False #key here is the old indices consistent in visibilities.
+
     # fig, axes = plt.subplots(figsize=(1600 / 100, 900 / 100), dpi=100)
     # axes.imshow(combined_image_pil)
     # axes.axis('off')
+    return combined_image_pil, visibilities_copy, new_color_mapping, new_corners_mapping
 
-    return combined_image_pil, visibilities_copy, color_mapping
-    # combined_image_pil.save('combined_image.png')
-def traverse(nusc, start,steps):
+
+def traverse(nusc, start, steps):
     cur_sample = start
     while steps > 0 and cur_sample["next"] != "":
         cur_sample = nusc.get("sample", cur_sample["next"])
         steps -= 1
     return cur_sample
 
+
 def main():
-    ROOT = "/bigdata/weizhen/metavqa_iclr/scenarios/nusc_real"
+    ROOT = "/bigdata/weizhen/metavqa_iclr/scenarios/nusc_real_2"
     nusc = NuScenes(version='v1.0-trainval', dataroot='/bigdata/datasets/nuscenes', verbose=True)
     nusc_scenes = nusc.scene
     EGO_SHAPE = (1.730, 4.084, 1.562)
@@ -160,11 +188,14 @@ def main():
         nbr_samples = nusc_scene["nbr_samples"]
         print(f"Total of {nbr_samples} frames in scene-{scene_idx}")
         frame_annotations = {}
+        scene_name = nusc_scene["name"]
+        #TODO create scene-consistent color mapping.
         while frame_idx < nusc_scene["nbr_samples"]:
             # Get CAM_FRONT data
             cam_front_sample_data = nusc.get("sample_data", sample["data"]["CAM_FRONT"])
             # Get the path to img, bboxes, and camera_instrinsics.
-            data_path, visible_boxes, camera_intrinsic = nusc.get_sample_data(cam_front_sample_data['token'],box_vis_level=BoxVisibility.ANY)
+            data_path, visible_boxes, camera_intrinsic = nusc.get_sample_data(cam_front_sample_data['token'],
+                                                                              box_vis_level=BoxVisibility.ANY)
             if len(visible_boxes) > 0:
                 original_boxes = nusc.get_boxes(cam_front_sample_data['token'])
                 # Get the sample_annotaion token associated with each box inside the frustum.
@@ -216,7 +247,7 @@ def main():
                     else:
                         medium_visible_indices.append(True)
                 # second, create instance_segmentation mask and filter out objects with less than 0.5 area visible in the image.
-                instance_seg, final_visible_indices, color_mapping = func(
+                instance_seg, final_visible_indices, color_mapping, corners_mapping = func(
                     nusc=nusc,
                     ego_pose_token=cam_front_sample_data["ego_pose_token"],
                     boxes=visible_boxes,
@@ -248,7 +279,7 @@ def main():
                         class_name=ALL_TYPE[instance_types[idx]],
                         visible=True,
                         observing_camera=["front"],
-                        collisions=[]
+                        collisions=[],
                     )
                     frame_data["objects"].append(data_point)
                 yaw = quaternion_yaw(Quaternion(ego_rotation))
@@ -258,7 +289,7 @@ def main():
                     heading=[np.cos(yaw), np.sin(yaw)],
                     pos=ego_translation[:2],
                     speed=0,
-                    bbox=visible_boxes_world_bottom[idx].tolist(),
+                    bbox=ego_box_world_bottom.tolist(),
                     type=ALL_TYPE["vehicle.ego"],
                     height=EGO_SHAPE[2],
                     class_name=ALL_TYPE["vehicle.ego"],
@@ -270,15 +301,19 @@ def main():
                 id2c = {
                     instance_tokens[key]: [round(c / 255, 5) for c in value] for key, value in color_mapping.items()
                 }
+                id2corners = {
+                    instance_tokens[key]: value for key, value in corners_mapping.items()
+                }
                 frame_annotations[frame_idx] = dict(
                     annotation=frame_data,
                     id2c=id2c,
                     instance_seg=instance_seg,
-                    rgb=Image.open(data_path)
+                    rgb=Image.open(data_path),
+                    id2corners=id2corners
                 )
+            print(f"Done {frame_idx} or {scene_name}")
             sample = traverse(nusc, sample, 1)
             frame_idx += 1
-        scene_name = nusc_scene["name"]
         episode_path = os.path.join(ROOT, f"{scene_name}_0_{frame_idx - 1}")
         for frame_idx, records in frame_annotations.items():
             identifier = f"{scene_idx}_{frame_idx}"
@@ -286,16 +321,20 @@ def main():
             frame_path = os.path.join(episode_path, identifier)
             os.makedirs(frame_path, exist_ok=True)
             id2c_path = os.path.join(frame_path, f"id2c_{identifier}.json")
+            id2corners_path = os.path.join(frame_path, f"id2corners_{identifier}.json")
             rgbf_path = os.path.join(frame_path, f"rgb_front_{identifier}.png")
             mask_path = os.path.join(frame_path, f"mask_front_{identifier}.png")
             world_path = os.path.join(frame_path, f"world_{identifier}.json")
             try:
                 json.dump(records["annotation"], open(world_path, "w"), indent=2)
                 json.dump(records["id2c"], open(id2c_path, "w"), indent=2)
+                json.dump(records["id2corners"], open(id2corners_path, "w"), indent=2)
                 records["rgb"].save(rgbf_path)
                 records["instance_seg"].save(mask_path)
             except Exception as e:
                 raise e
         assert sample["token"] == nusc_scene["last_sample_token"]
+
+
 if __name__ == "__main__":
     main()
