1fe2d883ab247c3e0005200a4442a31b0a2a37c6
weizhen <wwz011210@.ucla.edu>
2024-10-24 00:08:23 -0700

    Tested

---
+++ scripts_cvpr/nusc_sim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SAVE_DIR="/bigdata/weizhen/metavqa_cvpr/scenarios/nusc_sim"
+DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+METAVQA_DIR="$(dirname "$DIR")"
+CONFIG="${METAVQA_DIR%/}/som/configs/nusc_sim.yaml"
+START_IDX=0
+END_IDX=8
+NUM_PROC=4
+cd $METAVQA_DIR;
+python -m vqa.find_nuscenes_observation --headless --num_proc $NUM_PROC --config $CONFIG --start $START_IDX --end $END_IDX
+
