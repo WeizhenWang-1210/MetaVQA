b4e3a5b4fe217f40948c6e66e8a7b5773fdd9da8
weizhen <wwz011210@g.ucla.edu>
2025-04-29 12:25:04 -0700

    fix an error in pathing

--- scripts_cvpr/Scenario_gen/nusc_sim.sh
+++ scripts_cvpr/Scenario_gen/nusc_sim.sh
@@ -2,7 +2,7 @@
 SAVE_DIR="/bigdata/weizhen/metavqa_cvpr/scenarios/nusc_sim"
 DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 METAVQA_DIR="$(dirname "$DIR")"
-CONFIG="${METAVQA_DIR%/}/som/configs/nusc_sim.yaml"
+CONFIG="${METAVQA_DIR}/som/configs/nusc_sim.yaml"
 START_IDX=0
 END_IDX=400
 NUM_PROC=8
