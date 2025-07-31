8f74c956d1b4dcd05968c37ca4995b973771a552
weizhen <wwz011210@g.ucla.edu>
2025-07-31 14:31:19 -0700

    updated sample scripts

--- sample_scripts/test_scenegen.sh
+++ sample_scripts/test_scenegen.sh
@@ -2,31 +2,35 @@
 
 # 1 nusc real scenario annotations.
 # This is the root directory at which the generated scenarios are saved.
-# SAVE_DIR="/bigdata/weizhen/releases/MetaVQA_migrated/scenarios/nusc_real"
-# START_IDX=400
-# END_IDX=408
-# # Number of independent processed to use. This program is trivially parallelizable as no inter-process communication is needed.
-# # The number of processes should be equal to the number of CPU cores on your machine.
-# # The scenarios are saved independently, so you can run this script multiple times with different start and end indices.
-# NUM_PROC=4
-# DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-# METAVQA_DIR="$(dirname "$DIR")"
-# cd $METAVQA_DIR;
-# python -m vqa.scenegen.nusc_devkit_annotation --start $START_IDX --end $END_IDX --num_proc $NUM_PROC --store_dir $SAVE_DIR
-
-# # 2 nusc sim scenario annotations.
+# Remember to "$conda activate nusc" before running this script.
+SAVE_DIR="/bigdata/weizhen/releases/MetaVQA_migrated/scenarios/nusc_real"
+START_IDX=400
+END_IDX=408
+# Number of independent processed to use. This program is trivially parallelizable as no inter-process communication is needed.
+# The number of processes should be equal to the number of CPU cores on your machine.
+# The scenarios are saved independently, so you can run this script multiple times with different start and end indices.
+NUM_PROC=4
+DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+METAVQA_DIR="$(dirname "$DIR")"
+cd $METAVQA_DIR;
+python -m vqa.scenegen.nusc_devkit_annotation --start $START_IDX --end $END_IDX --num_proc $NUM_PROC --store_dir $SAVE_DIR
+ 
+# 2 nusc sim scenario annotations.
 # SAVE_DIR is specified in config.
-# DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-# METAVQA_DIR="$(dirname "$DIR")"
-# CONFIG="${METAVQA_DIR}/sample_scripts/scenegen_config/nusc_sim.yaml"
-# START_IDX=400
-# END_IDX=401
-# NUM_PROC=1
-# cd $METAVQA_DIR;
-# python -m vqa.scenegen.nusc_metadrive_annotation --headless --num_proc $NUM_PROC --config $CONFIG --start $START_IDX --end $END_IDX
+# Remember to "$conda activate metavqa" before running this script.
+
+DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+METAVQA_DIR="$(dirname "$DIR")"
+CONFIG="${METAVQA_DIR}/sample_scripts/scenegen_config/nusc_sim.yaml"
+START_IDX=400
+END_IDX=408
+NUM_PROC=4
+cd $METAVQA_DIR;
+python -m vqa.scenegen.nusc_metadrive_annotation --headless --num_proc $NUM_PROC --config $CONFIG --start $START_IDX --end $END_IDX
 
 
 # 3 waymo sim scenario annotations.
+# Remember to "$conda activate metavqa" before running this script.
 paths=(
     "/bigdata/datasets/scenarionet/waymo/training/training_1/"
 )
@@ -35,10 +39,9 @@ METAVQA_DIR="$(dirname "$DIR")"
 CONFIG="${METAVQA_DIR}/sample_scripts/scenegen_config/waymo_sim.yaml"
 SETTING="NORMAL"
 START_IDX=400
-END_IDX=401
-NUM_PROC=1
+END_IDX=408
+NUM_PROC=4
 export SETTING
-
 cd $METAVQA_DIR;
 for path in "${paths[@]}"; do
     if [ -d "$path" ]; then
--- sample_scripts/test_vqagen.sh
+++ sample_scripts/test_vqagen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # (1) Sample code for generating VQA on the nuScenes real scenarios
-SCENARIOS="/bigdata/weizhen/metavqa_release/scenarios/nusc_real"
-SAVEPATH="/bigdata/weizhen/metavqa_release/vqas/nusc_real/nusc_real.json"
-NUMPROC=1
+SCENARIOS="/bigdata/weizhen/releases/MetaVQA_migrated/scenarios/nusc_real"
+SAVEPATH="/bigdata/weizhen/releases/MetaVQA_migrated/vqas/nusc_real.json"
+NUMPROC=16
 DOMAIN="real"
 DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 METAVQA_DIR="$(dirname "$DIR")"
@@ -13,13 +13,13 @@ python -m som.static_question_generation --scenarios $SCENARIOS \
 
 # (2) Sample code for generating VQA on the WAYMO/nuScenes sim scenarios
 #!/bin/bash
-# SCENARIOS="/bigdata/weizhen/metavqa_release/scenarios/waymo_sim"
-# SAVEPATH="/bigdata/weizhen/metavqa_release/vqas/waymo_sim/waymo_sim.json"
-# NUMPROC=1
-# DOMAIN="sim"
-# DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-# METAVQA_DIR="$(dirname "$DIR")"
-# cd $METAVQA_DIR;
-# python -m closed_loop.static_question_generation --scenarios $SCENARIOS \
-#   --save_path $SAVEPATH --num_proc $NUMPROC \
-#   --domain $DOMAIN
+SCENARIOS="/bigdata/weizhen/releases/MetaVQA_migrated/scenarios/waymo_sim"
+SAVEPATH="/bigdata/weizhen/releases/MetaVQA_migrated/vqas/waymo_sim.json"
+NUMPROC=16
+DOMAIN="sim"
+DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+METAVQA_DIR="$(dirname "$DIR")"
+cd $METAVQA_DIR;
+python -m closed_loop.static_question_generation --scenarios $SCENARIOS \
+  --save_path $SAVEPATH --num_proc $NUMPROC \
+  --domain $DOMAIN
